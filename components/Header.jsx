ea66cbfece13fdd24bd29e70679c4f83c9954b4c
Gavin Yip <gavin.yip88@gmail.com>
2024-01-18 21:00:39 +0000

    header sytling

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,20 +2,39 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex gap-4 items-center w-full justify-start bg-green-200 p-4 fixed">
-      <Link href="/">
-        <p className="text-xl  text-white hover:underline
-        ">Home</p>
-      </Link>
-      <Link href="/about">
-        <p className="text-xl  text-white hover:underline">About</p>
-      </Link>
-      <Link href="/projects">
-        <p className="text-xl  text-white hover:underline">Projects</p>
-      </Link>
-      <Link href="/contact">
-        <p className="text-xl  text-white hover:underline">Contact</p>
-      </Link>
+    <header className="flex bg-green-200 items-center py-4 px-8 justify-between">
+      <nav className="flex just">
+        <Link href="/">
+          <p
+            className="text-xl  text-white hover:underline
+        "
+          >
+            Home
+          </p>
+        </Link>
+        <Link href="/about">
+          <p className="text-xl  text-white hover:underline">About</p>
+        </Link>
+        <Link href="/projects">
+          <p className="text-xl  text-white hover:underline">Projects</p>
+        </Link>
+        <Link href="/contact">
+          <p className="text-xl  text-white hover:underline">Contact</p>
+        </Link>
+      </nav>
+
+      <h2>Logo</h2>
+      <nav>
+        <Link href="/" target={"blank"}>
+          L
+        </Link>
+        <Link href="/" target={"blank"}>
+          L
+        </Link>
+        <Link href="/" target={"blank"}>
+          L
+        </Link>
+      </nav>
     </header>
   );
 };
