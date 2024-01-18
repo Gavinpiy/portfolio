201eb9222243c179d712b2ebf2e806b9a673272f
Gavin Yip <gavin.yip88@gmail.com>
2024-01-18 14:42:33 +0000

    removed old imports

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -1,8 +1,5 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import About from "./about/page";
-import Contact from "./contact/page";
-import Projects from "./projects/page";
+
 import React from "react";
 import Home from "./home/Home";
 
