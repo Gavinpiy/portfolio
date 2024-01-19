29b2f84ec8a776aab557fc157ee5c7a4d98eba2c
Gavin Yip <gavin.yip88@gmail.com>
2024-01-19 11:44:10 +0000

    vercel bugs

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-function Page({ Component, pageProps }) {
+function Page() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between font-montserrat`}
