cf589baea37d28cada608df966039fc5ed659389
Gavin Yip <gavin.yip88@gmail.com>
2024-01-24 10:36:22 +0000

    added photos

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import Project from "../../components/Project";
-import data from "../../components/data.json";
 
 const Projects = () => {
   return (
     <>
-      {data.map((project, index) => (
-        <Project
-          key={project.id}
-          title={project.title}
-          desc={project.description}
-          index={index}
-          alt={project.alt}
-          url={project.url}
-
-        />
-      ))}
+      <Project />
     </>
   );
 };
--- components/Project.jsx
+++ components/Project.jsx
@@ -1,44 +1,56 @@
 import React from "react";
 import Image from "next/image";
+import data from "./data.json";
 
-const Project = ({ title, desc }) => {
+const Project = ({ project, index }) => {
+  return (
+    <div className="grid grid-cols-10 items-center w-full h-[60vh] bg-green-200 justify-center rounded-lg shadow-xl">
+      {index % 2 === 0 ? (
+        <>
+          <div className="relative col-span-6 items-center justify-center border-solid border-black border-2 h-full w-full">
+            <Image
+              src={project.url ? project.url : "/images/placeholder.png"}
+              alt={project.alt}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="ml-14 col-span-4">
+            <h1 className="text-lg font-semibold">{project.title}</h1>
+            <p className="text-gray-600 pr-5">{project.description}</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="ml-14 col-span-4">
+            <h1 className="text-lg font-semibold">{project.title}</h1>
+            <p className="text-gray-600 pr-5">{project.description}</p>
+          </div>
+          <div className="relative col-span-6 items-center justify-center border-solid border-black border-2 h-full w-full">
+            <Image
+              src={project.url ? project.url : "/images/placeholder.png"}
+              alt={project.alt}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+const Projects = () => {
   return (
     <section className="flex flex-col items-center w-full py-4 pb-20 bg-gray-200">
       <h1 className="text-5xl font-bold p-4">Projects</h1>
-
-      <div className=" container bg-red-200 flex flex-col justify-center items-center w-full gap-20 p-10">
-        <div className="grid grid-cols-10 items-center w-full h-[50vh] bg-green-200 justify-center rounded-lg shadow-xl">
-          <div className="relative col-span-6 items-center justify-center border-solid border-black border-2 h-full w-full">
-            <Image
-              src="/placeholder.jpg"
-              alt=""
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-
-          <div className="ml-14 col-span-4">
-            <h1 className="text-lg font-semibold">{title}</h1>
-            <p className="text-gray-600">{desc}</p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-10 items-center w-full h-[50vh] bg-green-200 justify-center rounded-lg shadow-xl">
-          <div className="ml-14 col-span-4">
-            <h1 className="text-lg font-semibold">Project 2</h1>
-            <p className="text-gray-600">Project 2 description</p>
-          </div>
-          <div className="relative col-span-6 items-center justify-center border-solid border-black border-2 h-full w-full">
-            <Image
-              src="/placeholder.jpg"
-              alt=""
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </div>
+      <div className="container flex flex-col justify-center items-center w-full gap-20 p-10">
+        {data.map((project, index) => (
+          <Project key={project.id} project={project} index={index} />
+        ))}
       </div>
     </section>
   );
 };
-export default Project;
+
+export default Projects;
