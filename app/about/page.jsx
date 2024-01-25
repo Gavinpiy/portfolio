8ff60dfb157f0541547b10f11090d22547c31f65
Gavin Yip <gavin.yip88@gmail.com>
2024-01-25 15:09:05 +0000

    icons for tech

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,123 @@
 const About = () => {
   return (
-    <div className="py-20 flex gap-4 w-full justify-center font-montserrat">
+    <div className="py-20 flex flex-col justify-center items-center gap-4 w-full font-montserrat">
       <h1>About Me</h1>
-      <p>My name is John Doe. I am a web developer.</p>
+      <h2>Technologies and Tools</h2>
+
+      <p className="flex gap-8">
+        <a
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg"
+            width="75"
+            height="75"
+            alt="TypeScript"
+          />
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
+            width="75"
+            height="75"
+            alt="JavaScript"
+          />
+        </a>
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
+            width="75"
+            height="75"
+            alt="React"
+          />
+        </a>
+        <a href="https://nextjs.org/docs" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-colored.svg"
+            width="75"
+            height="75"
+            alt="NextJs"
+          />
+        </a>
+        <a
+          href="https://www.w3.org/TR/CSS/#css"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
+            width="75"
+            height="75"
+            alt="CSS3"
+          />
+        </a>
+        <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg"
+            width="75"
+            height="75"
+            alt="TailwindCSS"
+          />
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
+            width="75"
+            height="75"
+            alt="HTML5"
+          />
+        </a>
+        <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg"
+            width="75"
+            height="75"
+            alt="NodeJS"
+          />
+        </a>
+        <a href="https://expressjs.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg"
+            width="75"
+            height="75"
+            alt="Express"
+          />
+        </a>
+        <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg"
+            width="75"
+            height="75"
+            alt="PostgreSQL"
+          />
+        </a>
+        <a href="https://supabase.io/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/supabase-colored.svg"
+            width="75"
+            height="75"
+            alt="Supabase"
+          />
+        </a>
+        <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg"
+            width="75"
+            height="75"
+            alt="Figma"
+          />
+        </a>
+      </p>
     </div>
   );
 };
