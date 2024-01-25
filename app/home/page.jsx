d09c8537ad8b2bf21ce267aa9e62a8c63ad40fe2
Gavin Yip <gavin.yip88@gmail.com>
2024-01-25 23:04:24 +0000

    tweaks

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -3,7 +3,7 @@ import AnimateText from "../../components/AnimateText";
 const Home = () => {
   return (
     <>
-      <main className=" p-8 sm:p-8 md:p-18 lg:p-28 flex flex-col md:flex-row justify-center pt-18 items-center h-full font-montserrat sm:gap-10 lg:gap-20 pb-20">
+      <main className=" p-8 sm:p-8 md:p-18 lg:p-28 flex flex-col md:flex-row justify-center pt-18 items-center h-full font-montserrat sm:gap-10 md:gap-12 lg:gap-20 pb-20">
         <Image
           src="/images/placeholder.jpg"
           alt="Picture"
@@ -11,7 +11,6 @@ const Home = () => {
           height={500}
         />
         <div className="w-full md:w-4/5 gap-6 flex flex-col ">
-          {/* <AnimateText text="Hey there! I'm Gavin, a recent graduate from the School of Code bootcamp, and I'm excited to bring my unique blend of skills to the world of software development. With a background as a qualified pharmacist and a certified personal trainer, my journey has been anything but conventional. 💊💪" /> */}
           <p className="font-medium">
             Hey there! I'm Gavin, a recent graduate from the School of Code
             bootcamp, and I'm excited to bring my unique blend of skills to the
