abed3994903e77ae35518789506963a03e0c4e78
Gavin Yip <gavin.yip88@gmail.com>
2024-01-30 15:51:49 +0000

    icons typo on import

--- components/Project.jsx
+++ components/Project.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import data from "./data.json";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { GithubIcon } from "./icons";
+import { GithubIcon } from "./Icons";
 
 const Project = ({ project, index }) => {
   return (
