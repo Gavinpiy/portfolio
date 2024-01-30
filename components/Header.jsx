d7e90ffc0f7d1361da78fae2028bb7c56b8ce523
Gavin Yip <gavin.yip88@gmail.com>
2024-01-30 16:11:54 +0000

    bold header

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,10 +19,10 @@ const Header = () => {
   return (
     <header className="flex bg-[#4D7EA8] items-center py-4 lg:py-6 px-6 lg:px-10 justify-between w-full shadow-md ">
       <nav className="flex">
-        <CustomLink href="/" title="Home" className=" mr-2 lg:font-bold lg:mr-4 " />
-        <CustomLink href="/about" title="About" className="mr-2 lg:font-bold lg:mx-4" />
-        <CustomLink href="/projects" title="Projects " className="mr-2 lg:font-bold lg:mx-4" />
-        <CustomLink href="/contact" title="Contact" className="mr-2 lg:font-bold lg:mx-4" />
+        <CustomLink href="/" title="Home" className=" mr-2 font-bold lg:mr-4 " />
+        <CustomLink href="/about" title="About" className="mr-2 font-bold lg:mx-4" />
+        <CustomLink href="/projects" title="Projects " className="mr-2 font-bold lg:mx-4" />
+        <CustomLink href="/contact" title="Contact" className="mr-2 font-bold lg:mx-4" />
       </nav>
 
       <nav className=" flex items-center justify-center flex-wrap">
