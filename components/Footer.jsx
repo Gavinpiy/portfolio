6464f0d07f1c9432eb2bb8e9085fe1f711203ee9
Gavin Yip <gavin.yip88@gmail.com>
2024-01-30 16:22:13 +0000

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex bg-[#4D7EA8] items-center py-2 px-8 justify-between w-full">
+    <footer className="flex bg-[#4D7EA8] items-center py-1 px-8 justify-between w-full font-xs font-light">
       <p>© 2024 - Gavin Yip</p>
       <p>Created with Nextjs</p>
     </footer>
