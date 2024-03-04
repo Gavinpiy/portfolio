a4c2dc286b1663c530c761790bd5fdfafaabc838
Gavin Yip <gavin@Gavins-MBP.cust.communityfibre.co.uk>
2024-03-04 13:41:34 +0000

    bg

--- app/template.jsx
+++ app/template.jsx
@@ -6,36 +6,43 @@ const variants = {
   hidden: { opacity: 0, x: -20 },
   enter: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeInOut" } },
 };
-
+const bgVariants = {
+  hidden: { opacity: 0.5 },
+  visible: { opacity: 1, transition: { duration: 2 } },
+};
 export default function Template({ children }) {
   return (
-    <div
-      className="flex flex-col min-h-screen text-black"
-      style={{
-        backgroundImage: `url('/images/background.jpg')`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "repeat",
-        
-      }}
-    >
-      <header className="fixed top-0 w-full z-10">
-        <Header />
-      </header>
-      <main className="flex-grow pt-14 md:pt-10 lg:pt-20">
-        <motion.main
-          variants={variants}
-          initial="hidden"
-          animate="enter"
-          transition={{ type: "linear" }}
-          className=""
-        >
-          {children}
-        </motion.main>
-      </main>
-      <footer className=" w-full">
-        <Footer />
-      </footer>
-    </div>
+    
+      <div
+        className="flex flex-col min-h-screen text-black"
+        style={{
+          backgroundImage: `url('/images/background.jpg')`,
+          backgroundPosition: "center",
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "repeat",
+        }}
+        initial="hidden"
+        animate="visible"
+        variants={bgVariants}
+      >
+        <header className="fixed top-0 w-full z-10">
+          <Header />
+        </header>
+        <main className="flex-grow pt-14 md:pt-10 lg:pt-20">
+          <motion.main
+            variants={variants}
+            initial="hidden"
+            animate="enter"
+            transition={{ type: "linear" }}
+            className=""
+          >
+            {children}
+          </motion.main>
+        </main>
+        <footer className=" w-full">
+          <Footer />
+        </footer>
+      </div>
+    
   );
 }
